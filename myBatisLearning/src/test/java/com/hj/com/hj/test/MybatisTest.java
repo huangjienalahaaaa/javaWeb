7a890140fb8c61e0393b74edfba4fe798837135d
huangjie <137612270@qq.com>
2020-07-13 09:12:55 +0800

    f

--- myBatisLearning/src/test/java/com/hj/com/hj/test/MybatisTest.java
+++ myBatisLearning/src/test/java/com/hj/com/hj/test/MybatisTest.java
@@ -23,13 +23,6 @@ public class MybatisTest {
     /**
      * 入门案例：
      * 实现步骤：
-     *
-     * 1. 读取配置文件
-     * 2. 创建sqlSessionFactory工程
-     * 3. 使用工厂生产sqlSession对象
-     * 4. 使用sqlSession创建Dao接口的代理对象
-     * 5. 使用代理对象执行方法
-     * 6. 释放资源
      */
     public static void main(String[] args) throws Exception{
 
