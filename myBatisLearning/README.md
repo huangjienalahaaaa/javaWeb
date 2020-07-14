30b6b413ad775026da15dd20956f6ff81656d796
huangjie <137612270@qq.com>
2020-07-14 09:27:56 +0800

    f

--- myBatisLearning/README.md
+++ myBatisLearning/README.md
@@ -1,1 +1,18 @@
-# v2.2 不改变原来的方式，自己来写dao实现类，该怎么用
+# v3.0 自定义myBatis的分析-执行查询所有分析
+
+1. 对主配置文件(sqlMapConfig.xml)：
+
+![](img/1.jpg)
+
+
+
+2. 对映射配置文件(IUserDao.xml):
+![](img/2.jpg)
+
+3. 流程：
+![](img/3.jpg)
+![](img/4.jpg)
+![](img/5.jpg)
+![](img/6.jpg)
+![](img/7.jpg)
+![](img/8.jpg)
--- myBatisLearning/src/test/java/com/hj/com/hj/test/MybatisTest.java
+++ myBatisLearning/src/test/java/com/hj/com/hj/test/MybatisTest.java
@@ -1,7 +1,6 @@
 package com.hj.com.hj.test;
 
 import com.hj.dao.IUserDao;
-import com.hj.dao.impl.IserDaoImpl;
 import com.hj.domain.User;
 
 import java.io.InputStream;
@@ -14,32 +13,24 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 
-
-/**
- * @author hj
- * @create 2020-07-13 8:32
- * myBatis的入门案例
- */
 public class MybatisTest {
-    /**
-     * 入门案例：
-     * 实现步骤：
-     */
     public static void main(String[] args) throws Exception{
 
-
         InputStream in = Resources.getResourceAsStream("SqlMapConfig.xml");
+
         SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
         SqlSessionFactory factory = builder.build(in);
-//        3.使用工厂创建dao对象
-        IUserDao userDao = new IserDaoImpl(factory);
+
+        SqlSession session = factory.openSession();
+
+        IUserDao userDao = session.getMapper(IUserDao.class);
 
         List<User> users = userDao.findAll();
         for(User user : users){
             System.out.println(user);
         }
-//        //6.释放资源
-//        session.close();
+
+        session.close();
         in.close();
     }
 }
